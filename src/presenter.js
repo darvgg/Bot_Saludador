c51b853dd7b635403a7df40bb1f29655d625a418
Diego Rios <diegoarv08@hotmail.com>
2025-02-15 18:30:16 -0400

    Saludo con la edad

--- src/presenter.js
+++ src/presenter.js
@@ -8,9 +8,12 @@ const nombre_input = document.querySelector("#nombre-input")
 
 const op_genero_input = document.querySelector("#genero")
 
+const edad_input = document.querySelector("#edad-input")
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const nombre = nombre_input.value;
   const genero = op_genero_input.value;
-  div.innerHTML = "<p>" + saludar_hola(nombre,genero) + "</p>";
+  const edad = Number.parseInt(edad_input.value);
+  div.innerHTML = "<p>" + saludar_hola(nombre,genero,edad) + "</p>";
 });
--- src/saludador.js
+++ src/saludador.js
@@ -11,21 +11,32 @@ function saludar_hora(){
     return txt;
 }
 
-function saludar_genero(genero){
+function saludar_genero_edad(genero,edad){
     var pro_nombre;
-    if (genero === 'M')
-        pro_nombre= "Señor";
-    else if (genero === 'F')
-        pro_nombre= "Señora";
-    else 
-        pro_nombre= "Señore";
+    if (edad >= 30){
+        if (genero === 'M')
+            pro_nombre= "señor";
+        else if (genero === 'F')
+            pro_nombre= "señora";
+        else 
+            pro_nombre= "señore";
+    }
+    else{
+        if (genero === 'M')
+            pro_nombre= "jovencito";
+        else if (genero === 'F')
+            pro_nombre= "jovencita";
+        else 
+            pro_nombre= "jovencite";
+    }
+
     return pro_nombre;
 }
 
-function saludo(nombre, genero){
+function saludo(nombre, genero,edad){
     var mensaje;
     inicio_saludo=saludar_hora(hora);
-    pronom=saludar_genero(genero);
+    pronom=saludar_genero_edad(genero,edad);
     mensaje = inicio_saludo + " " + pronom + " " + nombre;
     return mensaje;
 }
