08038e880f1dc48989415af913501746d8f6ecd2
Diego Rios <diegoarv08@hotmail.com>
2025-02-15 17:57:58 -0400

    Saludo por hora

--- src/presenter.js
+++ src/presenter.js
@@ -2,15 +2,13 @@ import saludar_hola from "./saludador";
 
 const form =document.querySelector("#saludar-form")
 
-const saludar_boton = document.querySelector("#saludar-boton")
-
 const div = document.querySelector("#resultado-div")
 
 const nombre_input = document.querySelector("#nombre-input")
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const nombre= nombre_input.ariaValueMax;
+  const nombre= nombre_input.value;
   div.innerHTML = "<p>" + saludar_hola(nombre) + "</p>";
 
 });
--- src/saludador.js
+++ src/saludador.js
@@ -1,5 +1,12 @@
 function saludar_hola(nombre){
-    var txt = "Hola "+nombre
+    var txt;
+    const hora = 21//new Date().getHours()
+    if (hora<12)
+        txt= "Buenos dias "+nombre
+    else if (hora<20)
+        txt= "Buenas tardes "+nombre
+    else 
+        txt= "Buenos noches "+nombre
     return txt;
 }
 export default saludar_hola;
