fb0fa060f46fb8ac42886bfb40c7dd208ee5621d
Diego Rios <diegoarv08@hotmail.com>
2025-02-15 18:17:20 -0400

    Saludo con genero

--- src/presenter.js
+++ src/presenter.js
@@ -6,9 +6,11 @@ const div = document.querySelector("#resultado-div")
 
 const nombre_input = document.querySelector("#nombre-input")
 
+const op_genero_input = document.querySelector("#genero")
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const nombre= nombre_input.value;
-  div.innerHTML = "<p>" + saludar_hola(nombre) + "</p>";
-
+  const nombre = nombre_input.value;
+  const genero = op_genero_input.value;
+  div.innerHTML = "<p>" + saludar_hola(nombre,genero) + "</p>";
 });
--- src/saludador.js
+++ src/saludador.js
@@ -1,12 +1,32 @@
-function saludar_hola(nombre){
+const hora = new Date().getHours();
+
+function saludar_hora(){
     var txt;
-    const hora = 21//new Date().getHours()
     if (hora<12)
-        txt= "Buenos dias "+nombre
+        txt= "Buenos dias";
     else if (hora<20)
-        txt= "Buenas tardes "+nombre
+        txt= "Buenas tardes";
     else 
-        txt= "Buenos noches "+nombre
+        txt= "Buenos noches";
     return txt;
 }
-export default saludar_hola;
+
+function saludar_genero(genero){
+    var pro_nombre;
+    if (genero === 'M')
+        pro_nombre= "Señor";
+    else if (genero === 'F')
+        pro_nombre= "Señora";
+    else 
+        pro_nombre= "Señore";
+    return pro_nombre;
+}
+
+function saludo(nombre, genero){
+    var mensaje;
+    inicio_saludo=saludar_hora(hora);
+    pronom=saludar_genero(genero);
+    mensaje = inicio_saludo + " " + pronom + " " + nombre;
+    return mensaje;
+}
+export default saludo;
